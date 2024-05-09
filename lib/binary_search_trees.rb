efe5e81e66b691883c97b83fb3c4b415eae67b82
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-05-09 14:09:25 -0400

    Remove @sorted_array instance variable from the Tree class

--- lib/binary_search_trees.rb
+++ lib/binary_search_trees.rb
@@ -16,15 +16,14 @@ class Tree
   attr_accessor :root
 
   def initialize(array)
-    @sorted_arr = array.sort.uniq
-    @root = build_tree(@sorted_arr)
+    @root = build_tree(array.sort.uniq)
   end
 
   def build_tree(array)
     mid = (array.length / 2)
-    root = Node.new(array[mid])
+    root = Node.new(array[mid]) unless array[mid].nil?
 
-    if mid > 0
+    if mid.positive?
       root.left = build_tree(array.values_at(0..(mid - 1)))
       root.right = build_tree(array.values_at((mid + 1)..-1))
     end
@@ -32,13 +31,13 @@ class Tree
     root
   end
 
-  def pretty_print(node = @root, prefix = '', is_left = true)
-    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+  def pretty_print(node = @root, prefix = '', is_left: true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", is_left: false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", is_left: true) if node.left
   end
 
-  def insert(root = @root, value)
+  def insert(value, root = @root)
     node = Node.new(value)
 
     if root.nil?
@@ -47,13 +46,47 @@ class Tree
       return root if root.data == value
 
       if root.data < value
-        root.right = insert(root.right, value)
-      else 
-        root.left = insert(root.left, value)
+        root.right = insert(value, root.right)
+      else
+        root.left = insert(value, root.left)
       end
     end
-    
+
     root
+  end
+
+  def delete(value, root = @root)
+    return root if root.nil?
+
+    if value < root.data
+      root.left = delete(value, root.left)
+    elsif value > root.data
+      root.right = delete(value, root.right)
+    else
+      if root.left.nil?
+        return root.right
+      elsif root.right.nil?
+        return root.left
+      end
+
+      root.data = min_value(root.right)
+      root.right = delete(root.data, root.right)
+    end
+
+    root
+  end
+
+  private
+
+  def min_value(root)
+    min_val = root.data
+
+    until root.left.nil?
+      min_val = root.left.data
+      root = root.left
+    end
+
+    min_val
   end
 end
 
@@ -62,3 +95,8 @@ array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 binary_search_tree = Tree.new(array)
 binary_search_tree.insert(1000)
 binary_search_tree.pretty_print
+binary_search_tree.delete(1)
+binary_search_tree.pretty_print
+binary_search_tree.insert(2)
+binary_search_tree.pretty_print
+p binary_search_tree
