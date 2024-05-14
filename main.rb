72b1964e411f7133517e775a4d2307b5331854ef
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-05-14 04:04:28 -0400

    Write driver script as per project instructions

--- main.rb
+++ main.rb
@@ -2,15 +2,21 @@
 
 require_relative 'lib/tree'
 
-array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-
-binary_search_tree = Tree.new(array)
-binary_search_tree.insert(1000)
-binary_search_tree.insert(1500)
-binary_search_tree.insert(15)
-binary_search_tree.insert(10)
-binary_search_tree.pretty_print
-p binary_search_tree.balanced?
-binary_search_tree.rebalance
-binary_search_tree.pretty_print
-p binary_search_tree.balanced?
+tree = Tree.new(Array.new(15) { rand(1..100) })
+tree.pretty_print
+p tree.balanced?
+p tree.level_order
+p tree.preorder
+p tree.postorder
+p tree.inorder
+tree.insert(101)
+tree.insert(102)
+tree.pretty_print
+p tree.balanced?
+tree.rebalance
+tree.pretty_print
+p tree.balanced?
+p tree.level_order
+p tree.preorder
+p tree.postorder
+p tree.inorder
