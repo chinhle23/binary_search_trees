6b4c4ec47030efba61b5fd4a4bfec5caa403ed4c
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-05-14 03:26:57 -0400

    Write #rebalance method

--- lib/binary_search_trees.rb
+++ lib/binary_search_trees.rb
@@ -210,6 +210,10 @@ class Tree
     true
   end
 
+  def rebalance
+    @root = build_tree(inorder)
+  end
+
   private
 
   def min_value(root)
@@ -228,7 +232,11 @@ array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
 binary_search_tree = Tree.new(array)
 binary_search_tree.insert(1000)
+binary_search_tree.insert(1500)
+binary_search_tree.insert(15)
+binary_search_tree.insert(10)
 binary_search_tree.pretty_print
-p binary_search_tree.height(binary_search_tree.find(6346))
-p binary_search_tree.height(binary_search_tree.find(324))
+p binary_search_tree.balanced?
+binary_search_tree.rebalance
+binary_search_tree.pretty_print
 p binary_search_tree.balanced?
